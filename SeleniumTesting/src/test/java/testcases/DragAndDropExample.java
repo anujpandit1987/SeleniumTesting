cf8c30e3e1b00a4dbca849d0356d713ca0198d07
anujpandit1987 <anujpandit1987@gmail.com>
2021-05-23 20:37:45 +0530

    Browser driver changed

--- SeleniumTesting/src/test/java/testcases/DragAndDropExample.java
+++ SeleniumTesting/src/test/java/testcases/DragAndDropExample.java
@@ -6,15 +6,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 
 
 public class DragAndDropExample {
 	
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "D:\\Croma\\Selenium\\chromedriver.exe");
-			
-		WebDriver driver = new ChromeDriver();
+System.setProperty("webdriver.gecko.driver", "D:\\Selenium\\geckodriver\\geckodriver.exe");
+		
+		
+		WebDriver driver = new FirefoxDriver();
 		driver.get("https://jqueryui.com/droppable/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
